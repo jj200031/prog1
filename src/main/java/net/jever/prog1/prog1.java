f1562548a888afc345be705147d7fa61b6db2d1f
jj200031 <jaxeverh@gmail.com>
2022-12-07 09:21:37 -0500

    New Blocks

--- src/main/java/net/jever/prog1/prog1.java
+++ src/main/java/net/jever/prog1/prog1.java
@@ -1,10 +1,13 @@
 package net.jever.prog1;
 
 import com.mojang.logging.LogUtils;
+import net.jever.prog1.block.ModBlocks;
+import net.jever.prog1.item.ModItems;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.Blocks;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.RegistryEvent;
+import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.InterModComms;
 import net.minecraftforge.fml.common.Mod;
@@ -26,8 +29,11 @@ public class prog1 {
 
 
     public prog1() {
-        // Register the setup method for modloading
-        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
+        IEventBus eventBus = FMLJavaModLoadingContext.get().getModEventBus();
+        ModItems.register(eventBus);
+        ModBlocks.register(eventBus);
+
+        eventBus.addListener(this::setup);
         // Register ourslves for server and other game events we are interested in
         MinecraftForge.EVENT_BUS.register(this);
     }
