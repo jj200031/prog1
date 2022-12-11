03ba5a59c450a84362024476e46198c7d38f7909
jj200031 <jaxeverh@gmail.com>
2022-12-10 22:09:44 -0500

    ADD still smeltery gui

--- src/main/java/net/jever/prog1/screen/StillSmelteryMenu.java
+++ src/main/java/net/jever/prog1/screen/StillSmelteryMenu.java
@@ -8,14 +8,12 @@ import net.minecraft.world.entity.player.Inventory;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.inventory.AbstractContainerMenu;
 import net.minecraft.world.inventory.ContainerLevelAccess;
-import net.minecraft.world.inventory.MenuType;
 import net.minecraft.world.inventory.Slot;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraftforge.items.CapabilityItemHandler;
 import net.minecraftforge.items.SlotItemHandler;
-import org.jetbrains.annotations.Nullable;
 
 public class StillSmelteryMenu extends AbstractContainerMenu {
     private final StillSmelteryBlockEntity blockEntity;
@@ -27,7 +25,7 @@ public class StillSmelteryMenu extends AbstractContainerMenu {
     }
     public StillSmelteryMenu(int pContainerId, Inventory inv, BlockEntity entity) {
         super(ModMenuTypes.STILL_SMELTERY_MENU.get(), pContainerId);
-        checkContainerSize(inv, 3);
+        checkContainerSize(inv, 4);
         blockEntity = ((StillSmelteryBlockEntity) entity);
         this.level = inv.player.level;
 
@@ -35,9 +33,9 @@ public class StillSmelteryMenu extends AbstractContainerMenu {
         addPlayerHotbar(inv);
 
         this.blockEntity.getCapability(CapabilityItemHandler.ITEM_HANDLER_CAPABILITY).ifPresent(handler -> {
-            this.addSlot(new SlotItemHandler(handler, 0, 56, 17));
-            this.addSlot(new SlotItemHandler(handler, 1, 56, 53));
-            this.addSlot(new ModResultSlot(handler, 2, 116, 35));
+            this.addSlot(new SlotItemHandler(handler, 0, 28, 34));
+            this.addSlot(new SlotItemHandler(handler, 1, 67, 34));
+            this.addSlot(new ModResultSlot(handler, 2, 125, 35));
         });
     }
 
